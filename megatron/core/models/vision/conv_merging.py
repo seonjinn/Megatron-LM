1ae7e4b3b4bdd7c206267a78aa549701364bfdfc
Tyler Poon <tpoon@nvidia.com>
2025-06-13 11:41:59 -0700

    conv_merging fp8 changes

--- megatron/core/models/vision/conv_merging.py
+++ megatron/core/models/vision/conv_merging.py
@@ -3,6 +3,7 @@ import torch
 from megatron.core.tensor_parallel.layers import ColumnParallelLinear, RowParallelLinear
 from megatron.core.transformer.mlp import MLP, MLPSubmodules
 from megatron.core.transformer.module import MegatronModule
+from megatron.core.fp8_utils import get_fp8_context
 
 try:
     from megatron.core.extensions.transformer_engine import (
@@ -22,14 +23,16 @@ class ConvTokenMerge(MegatronModule):
         # TODO: should padding?
         self.hidden_size = config.hidden_size
         self.size = size
-        self.mlp = MLP(
-            config,
-            submodules=MLPSubmodules(
-                linear_fc1=TEColumnParallelLinear if use_te else ColumnParallelLinear,
-                linear_fc2=TERowParallelLinear if use_te else RowParallelLinear,
-            ),
-            input_size=self.hidden_size * self.size * self.size,
-        )
+        fp8_init_context = get_fp8_context(config, 0, is_init=True)
+        with fp8_init_context:
+            self.mlp = MLP(
+                config,
+                submodules=MLPSubmodules(
+                    linear_fc1=TEColumnParallelLinear if use_te else ColumnParallelLinear,
+                    linear_fc2=TERowParallelLinear if use_te else RowParallelLinear,
+                ),
+                input_size=self.hidden_size * self.size * self.size,
+            )
 
     def create_indices(self, patch_sizes):
         assert all(
@@ -56,13 +59,25 @@ class ConvTokenMerge(MegatronModule):
         ), "ConvTokenMerge requires image patch sizes to be divisible by 2"
 
         indices = self.create_indices(patch_sizes)
-        inverse_indices = torch.argsort(indices)
 
         new_x = x[:, indices, :]
-        new_x, bias = self.mlp(
-            new_x.view(new_x.shape[0], -1, self.hidden_size * self.size * self.size)
-        )
-        if bias is not None:
-            new_x = new_x + bias
+
+        # Padding for fp8
+        fp8_padding = 0
+        pad_to_value = 16 * self.size * self.size
+        if self.config.fp8 and new_x.shape[1] % pad_to_value != 0:
+            fp8_padding = pad_to_value - (new_x.shape[1] % pad_to_value)
+            new_x = torch.cat([new_x, torch.zeros([new_x.shape[0], fp8_padding, new_x.shape[2]]).to(new_x.device).to(new_x.dtype)], dim=1)
+
+        fp8_context = get_fp8_context(self.config) if self.config.fp8 else nullcontext()
+        with fp8_context:
+            new_x, bias = self.mlp(
+                new_x.view(new_x.shape[0], -1, self.hidden_size * self.size * self.size)
+            )
+            if bias is not None:
+                new_x = new_x + bias
+
+        if fp8_padding > 0:
+            new_x = new_x[:, :-(fp8_padding // (self.size * self.size)), :]
 
         return new_x
