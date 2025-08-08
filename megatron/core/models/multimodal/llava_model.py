f69fc7ff477572639b47db5c92234287c52232d6
Natan Bagrov <nbagrov@nvidia.com>
2025-08-08 10:55:32 -0700

    ADLR/megatron-lm!3779 - Added `num_frames` to `VLMForwardStep` to support generation (evaluation)
    Co-authored-by: Eugene Khvedchenia <ekhvedchenia@nvidia.com>

--- megatron/core/models/multimodal/llava_model.py
+++ megatron/core/models/multimodal/llava_model.py
@@ -436,8 +436,6 @@ class LLaVAModel(MegatronModule):
             raise NotImplementedError("EVS does not support dynamic resolution, yet")
         if self.image_break_token is not None:
             raise NotImplementedError("EVS does not support image break token, yet")
-        if self._vision_fp8 or self._vision_fp8_no_arch:
-            raise NotImplementedError("EVS does not support fp8 training, yet")
         return evs
 
     def shared_embedding_or_output_weight(self):
@@ -754,7 +752,7 @@ class LLaVAModel(MegatronModule):
             assert final_retention_mask.sum() == vision_tokens_retention_mask.sum() + text_position_ids.numel()
 
             initial_packed_seq_params = copy.deepcopy(packed_seq_params) if packed_seq_params is not None else None
-            shard_factor = self._calc_shard_factor()
+            shard_factor = self._calc_shard_factor() if self.training else None
 
             final_embedding, final_position_ids, packed_seq_params = self.efficient_video_sampler.mask_embeddings(
                 embeddings=final_embedding, evs_mask=final_retention_mask, packed_seq_params=initial_packed_seq_params, pad_to_divisibility=shard_factor
@@ -1210,9 +1208,10 @@ class LLaVAModel(MegatronModule):
                 )
                 # For simplicity of dealing with list of images, we concat all
                 image_tokens_retention_mask = torch.cat(evs_masks, dim=0)
-                # assert len(evs_masks) == len(media_types)
+                _, actual_mask_seqlen = image_tokens_retention_mask.shape
                 assert sum(map(len, evs_masks)) == len(images)
-
+                if masks_seqlen != actual_mask_seqlen:
+                    raise ValueError(f"Mismatch between actual EVS mask seqlen ({actual_mask_seqlen}) and expected EVS mask seqlen ({masks_seqlen}) ")
                 if masks_seqlen != image_embeddings.shape[0]:
                     raise ValueError(f"Mismatch between EVS mask seqlen ({masks_seqlen}) to the image embeddings seqlen ({image_embeddings.shape[0]})")
 
