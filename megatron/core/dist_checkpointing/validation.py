fdee7e06f578bcf9f6c02a6e0ccf3f6a890ea41e
yaoyu-33 <yaoyu.094@gmail.com>
2025-08-11 14:31:30 -0700

    update for nemo rl
    (cherry picked from commit ffb0c14359bdc21056ef7b293004309c1992285a)

--- megatron/core/dist_checkpointing/validation.py
+++ megatron/core/dist_checkpointing/validation.py
@@ -375,12 +375,11 @@ def maybe_report_missing_and_unexpected_keys(
 def _validate_common_state_dict(common_state_dict: CommonStateDict) -> None:
     """Validate consistancy across ranks for the common state dict
 
-    We save the common state dict only on rank 0. We validate to make sure that the common dict is consistant across ranks before saving.
+    We save the common state dict only on rank 0. We validate to make sure that the common dict is consistent across ranks before saving.
 
     Args:
         common_state_dict: The common state dict present in all ransk
     """
-
     if not torch.distributed.is_initialized():
         return
 
@@ -510,7 +509,7 @@ def _validate_sharding_for_key_flattened(tensors_by_shard):
         all_slices.append((sharding.flattened_range.start, sharding.flattened_range.stop))
 
     starts, stops = map(np.asarray, zip(*sorted(all_slices)))
-    expected_size = np.prod(local_shape)
+    expected_size = np.product(local_shape)
     if starts[0] != 0 or stops[-1] != expected_size or not np.all(starts[1:] == stops[:-1]):
         raise CheckpointingException(
             f"Flattened ranges dont cover the whole shard {tensors_by_shard[0]} of size {expected_size}. Ranges: {(starts, stops)}"
