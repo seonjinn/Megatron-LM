f89ad76b222bd1b6347c2053476d30e426b9754d
Matthieu Le <matthieul@nvidia.com>
2025-08-22 14:08:38 -0700

    Fix n5p5_mcore_to_hf

--- examples/multimodal/v2/n5p5_mcore_to_hf.sh
+++ examples/multimodal/v2/n5p5_mcore_to_hf.sh
@@ -5,7 +5,6 @@ MODEL_TYPE=${4:-12b}
 
 mkdir -p $HF_BASE_PATH
 HF_PATH=$HF_BASE_PATH/mcore_to_hf
-FIXED_HF_PATH=$HF_BASE_PATH/mcore_to_hf_fixed
 
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
@@ -20,9 +19,6 @@ python tools/checkpoint/convert.py \
     --max-queue-size 1 \
     --ckpt-step $CKPT_STEP
 
-# Step 2: Modify cls token
-python examples/multimodal/tools/modify_cls_token.py $HF_PATH $FIXED_HF_PATH
-
 # Create a txt file in $HF_BASE_PATH saying
 # original mcore path: $MCORE_PATH at iteration $CKPT_STEP
 touch $HF_BASE_PATH/mcore_to_hf_info.txt
@@ -30,9 +26,9 @@ echo "original mcore path: $MCORE_PATH at iteration $CKPT_STEP" >> $HF_BASE_PATH
 
 # copy the configuration based on the model type, error out if not 9b or 12b
 if [ "$MODEL_TYPE" == "9b" ]; then
-    cp /lustre/fsw/portfolios/llmservice/users/matthieul/repos_rebase/n5p5_9b_model_config/* $FIXED_HF_PATH
+    cp /lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/n5p5_9b_model_config/* $HF_PATH
 elif [ "$MODEL_TYPE" == "12b" ]; then
-    cp /lustre/fsw/portfolios/llmservice/users/charlwang/nvwork/250709_vlm/vision_model_config/* $FIXED_HF_PATH
+    cp /lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/n5p5_12b_hf_config/* $HF_PATH
 else
     echo "Error: MODEL_TYPE must be either '9b' or '12b', but got '$MODEL_TYPE'"
     exit 1
