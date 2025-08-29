9959819adb2693cf9ed6f0c7934e17f02c4191e6
Amala Sanjay Deshmukh <amalasanjayd@nvidia.com>
2025-08-29 13:03:36 -0700

    Add script for combining llm and vision weights

--- examples/multimodal/tools/prepare_llm.py
+++ examples/multimodal/tools/prepare_llm.py
@@ -1,82 +1,77 @@
 import torch
 import os
 import matplotlib.pyplot as plt
+import argparse
 
-import sys
-sys.path.append("/lustre/fsw/portfolios/llmservice/users/matthieul/repos_rebase/megatron-lm-vlm-hybrid-packing")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-dir", type=str, required=True)
+    parser.add_argument("--output-dir", type=str, required=True)
+    parser.add_argument("--tp", type=int, required=True)
+    parser.add_argument("--iter", type=int, required=True)
 
+    args = parser.parse_args()
 
-# orig_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nemotron5p5_hybrid_12b_tp_4/torch"
-# new_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nemotron5p5_hybrid_12b_patch_vocab_tp_4"
+    iter_dir = f"iter_{args.iter:07d}"
 
-# orig_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nemotron_5p5_12b_sft_11600_0711_tp_8/torch"
-# new_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nemotron_5p5_12b_sft_11600_0711_patch_vocab_tp_8"
+    # We're patching the embeddings weights from size 131072 to size 131584
+    if args.tp == 8:
+        num_pad = 132096 - 131072
+    elif args.tp == 4:
+        # num_pad = 131584 - 131072
+        num_pad = 132096 - 131072
 
-orig_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nano-v2-sft-lr5e-6-128k-nollama-thinkfix-ep2-iter_0006000-tp_8/torch"
-new_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nano-v2-sft-lr5e-6-128k-nollama-thinkfix-ep2-iter_0006000-path-vocab-tp_8"
+    # First, load all the embeddings and concatenate them.
+    input_embeddings = []
+    output_embeddings = []
+    for i in range(args.tp):
+        orig_path = os.path.join(args.input_dir, iter_dir, f"mp_rank_0{i}", "model_optim_rng.pt")
+        state_dict = torch.load(orig_path, weights_only=False)
+        input_embeddings.append(state_dict["model"]["embedding.word_embeddings.weight"].cpu())
+        output_embeddings.append(state_dict["model"]["output_layer.weight"].cpu())
 
-TP = 8
-iter_dir = "iter_0006000"
+    input_embeddings = torch.concat(input_embeddings)
+    output_embeddings = torch.concat(output_embeddings)
 
-# We're patching the embeddings weights from size 131072 to size 131584
-if TP == 8:
-    num_pad = 132096 - 131072
-elif TP == 4:
-    # num_pad = 131584 - 131072
-    num_pad = 132096 - 131072
+    plt.plot(input_embeddings.to(torch.float32).numpy().mean(axis=1)[:2000])
+    plt.savefig("input_embeddings.png")
+    plt.close()
+    plt.plot(output_embeddings.to(torch.float32).numpy().mean(axis=1)[:2000])
+    plt.savefig("output_embeddings.png")
+    plt.close()
 
-# First, load all the embeddings and concatenate them.
-input_embeddings = []
-output_embeddings = []
-for i in range(TP):
-    orig_path = os.path.join(orig_dir, iter_dir, f"mp_rank_0{i}", "model_optim_rng.pt")
-    state_dict = torch.load(orig_path, weights_only=False)
-    input_embeddings.append(state_dict["model"]["embedding.word_embeddings.weight"].cpu())
-    output_embeddings.append(state_dict["model"]["output_layer.weight"].cpu())
+    plt.plot(input_embeddings.to(torch.float32).numpy().mean(axis=1)[:2000])
+    plt.savefig("input_embeddings_patched.png")
+    plt.close()
+    plt.plot(output_embeddings.to(torch.float32).numpy().mean(axis=1)[:2000])
+    plt.savefig("output_embeddings_patched.png")
+    plt.close()
 
-input_embeddings = torch.concat(input_embeddings)
-output_embeddings = torch.concat(output_embeddings)
+    # Padd the embeddings with new random embeddings at the end of the embedding table.
+    input_embeddings_padded = torch.concat((
+        input_embeddings, torch.randn(num_pad, input_embeddings.shape[1]) * input_embeddings.std() + input_embeddings.mean()
+    )).to(input_embeddings.dtype)
+    output_embeddings_padded = torch.concat((
+        output_embeddings, torch.randn(num_pad, output_embeddings.shape[1]) * output_embeddings.std() + output_embeddings.mean()
+    )).to(output_embeddings.dtype)
 
-plt.plot(input_embeddings.to(torch.float32).numpy().mean(axis=1)[:2000])
-plt.savefig("input_embeddings.png")
-plt.close()
-plt.plot(output_embeddings.to(torch.float32).numpy().mean(axis=1)[:2000])
-plt.savefig("output_embeddings.png")
-plt.close()
+    # Save the chunked padded embeddings.
+    for i in range(args.tp):
+        orig_path = os.path.join(args.input_dir, iter_dir, f"mp_rank_0{i}", "model_optim_rng.pt")
+        state_dict = torch.load(orig_path, weights_only=False)
 
-# input_embeddings[:1000, :] = torch.randn(1000, input_embeddings.shape[1]) * input_embeddings[1000:, :].std() + input_embeddings[1000:, :].mean()
-# output_embeddings[:1000, :] = torch.randn(1000, output_embeddings.shape[1]) * output_embeddings[1000:, :].std() + output_embeddings[1000:, :].mean()
+        args.output_dir_tp = os.path.join(args.output_dir, iter_dir, f"mp_rank_0{i}")
+        os.makedirs(args.output_dir_tp, exist_ok=True)
+        new_path = os.path.join(args.output_dir_tp, "model_optim_rng.pt")
 
-plt.plot(input_embeddings.to(torch.float32).numpy().mean(axis=1)[:2000])
-plt.savefig("input_embeddings_patched.png")
-plt.close()
-plt.plot(output_embeddings.to(torch.float32).numpy().mean(axis=1)[:2000])
-plt.savefig("output_embeddings_patched.png")
-plt.close()
+        start = int(i * input_embeddings_padded.shape[0]/args.tp)
+        end = int((i+1) * input_embeddings_padded.shape[0]/args.tp)
 
-# Padd the embeddings with new random embeddings at the end of the embedding table.
-input_embeddings_padded = torch.concat((
-    input_embeddings, torch.randn(num_pad, input_embeddings.shape[1]) * input_embeddings.std() + input_embeddings.mean()
-)).to(input_embeddings.dtype)
-output_embeddings_padded = torch.concat((
-    output_embeddings, torch.randn(num_pad, output_embeddings.shape[1]) * output_embeddings.std() + output_embeddings.mean()
-)).to(output_embeddings.dtype)
+        state_dict["model"]["embedding.word_embeddings.weight"] = input_embeddings_padded[start:end, :]
+        state_dict["model"]["output_layer.weight"] = output_embeddings_padded[start:end, :]
 
-# Save the chunked padded embeddings.
-for i in range(TP):
-    orig_path = os.path.join(orig_dir, iter_dir, f"mp_rank_0{i}", "model_optim_rng.pt")
-    state_dict = torch.load(orig_path, weights_only=False)
+        print(input_embeddings_padded[start:end, :].shape)
+        print(output_embeddings_padded[start:end, :].shape)
+        torch.save(state_dict, new_path)
 
-    new_dir_tp = os.path.join(new_dir, iter_dir, f"mp_rank_0{i}")
-    os.makedirs(new_dir_tp, exist_ok=True)
-    new_path = os.path.join(new_dir_tp, "model_optim_rng.pt")
-
-    start = int(i * input_embeddings_padded.shape[0]/TP)
-    end = int((i+1) * input_embeddings_padded.shape[0]/TP)
-
-    state_dict["model"]["embedding.word_embeddings.weight"] = input_embeddings_padded[start:end, :]
-    state_dict["model"]["output_layer.weight"] = output_embeddings_padded[start:end, :]
-
-    print(input_embeddings_padded[start:end, :].shape)
-    print(output_embeddings_padded[start:end, :].shape)
-    torch.save(state_dict, new_path)
+    print(f"Saved patched embeddings to {args.output_dir}")
--- examples/multimodal/tools/replace_llm_backbone.py
+++ examples/multimodal/tools/replace_llm_backbone.py
@@ -1,74 +1,46 @@
 import torch
 import os
-import sys
-sys.path.append("/lustre/fsw/portfolios/llmservice/users/matthieul/repos_rebase/megatron-lm-vlm-hybrid-packing")
+import argparse
 
-# orig_dir = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/N5p5_phase3_blend2_torch_cradio_vlm_v1_rc3_tp8_reinit_patched-no-extra-state"
-# new_dir = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/nemotron5p5_hybrid_12b_patch_vocab_cradio_vlm_v1_rc3_tp8"
-# llm_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nemotron5p5_hybrid_12b_patch_vocab_tp_8/iter_1600000"
-# prefix = "language_model."
-# TP = 8
+orig_dir_TP_8 = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/N5p5_phase3_blend2_torch_cradio_vlm_v1_rc3_tp8_reinit_patched-no-extra-state"
+orig_dir_TP_4 = "/lustre/fs1/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/llama_3p1_8b_c-radio-vlm_v1_rc3-no-extra-state"
 
-# orig_dir = "/lustre/fs1/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/llama_3p1_8b_c-radio-vlm_v1_rc3-no-extra-state"
-# new_dir = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/nemotron5p5_hybrid_12b_patch_vocab_cradio_vlm_v1_rc3_tp4"
-# llm_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nemotron5p5_hybrid_12b_patch_vocab_tp_4/iter_1600000"
-# prefix = "language_model."
-# TP = 4
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-dir", type=str, required=True)
+    parser.add_argument("--output-dir", type=str, required=True)
+    parser.add_argument("--tp", type=int, required=True)
+    parser.add_argument("--iter", type=int, required=True)
 
-# orig_dir = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/N5p5_phase3_blend2_torch_cradio_vlm_v1_rc3_tp8_reinit_patched-no-extra-state"
-# new_dir = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/nemotron5p5_hybrid_12b_0701_cradio_vlm_v1_rc3_tp8"
-# llm_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nemotron5p5_hybrid_12b_tp_8_dq_patch_vocab_0701/iter_0000001"
-# prefix = "language_model."
-# TP = 8
+    args = parser.parse_args()
+    prefix = "language_model."
+    if args.tp == 8:
+        orig_dir = orig_dir_TP_8
+    elif args.tp == 4:
+        orig_dir = orig_dir_TP_4
+    else:
+        raise ValueError(f"Invalid tp: {args.tp}")
 
-# orig_dir = "/lustre/fs1/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/llama_3p1_8b_c-radio-vlm_v1_rc3-no-extra-state"
-# new_dir = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/nemotron5p5_hybrid_12b_0701_cradio_vlm_v1_rc3_tp4"
-# llm_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nemotron5p5_hybrid_12b_tp_4_dq_patch_vocab_0701/iter_0000001"
-# prefix = "language_model."
-# TP = 4
+    iter_dir = f"iter_{args.iter:07d}"
 
-# orig_dir = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/qwen2.5-7B-instruct-cradio-v3-g-mcore-tp4"
-# new_dir = "/lustre/fs1/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/llama_3p1_8b_c-radio-g-v3-no-extra-state"
-# llm_dir = "/lustre/fs1/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/llama_3p1_8b_c-radio-vlm_v1_rc3-no-extra-state/iter_0000001/"
-# prefix = ""
-# TP = 4
+    for i in range(args.tp):
+        orig_path = os.path.join(orig_dir, iter_dir, f"mp_rank_0{i}", "model_optim_rng.pt")
+        new_path = os.path.join(args.output_dir, iter_dir, f"mp_rank_0{i}", "model_optim_rng.pt")
+        os.makedirs(os.path.join(args.output_dir, iter_dir, f"mp_rank_0{i}"), exist_ok=True)
+        llm_path = os.path.join(args.input_dir, iter_dir, f"mp_rank_0{i}", "model_optim_rng.pt")
 
-# orig_dir = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/N5p5_phase3_blend2_torch_cradio_vlm_v1_rc3_tp8_reinit_patched-no-extra-state"
-# new_dir = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/nemotron_5p5_12b_sft_11600_0711_cradio_vlm_v1_rc3_tp8"
-# llm_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nemotron_5p5_12b_sft_11600_0711_patch_vocab_tp_8/iter_0011600"
-# prefix = "language_model."
-# TP = 8
+        state_dict = torch.load(orig_path, weights_only=False)
+        new_state_dict = state_dict.copy()
+        new_state_dict["model"] = dict()
 
-orig_dir = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/N5p5_phase3_blend2_torch_cradio_vlm_v1_rc3_tp8_reinit_patched-no-extra-state"
-new_dir = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/n5p5_12b_sft_0718_cradio_vlm_v1_rc3_tp8"
-llm_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nano-v2-sft-lr5e-6-128k-nollama-thinkfix-ep2-iter_0006000-path-vocab-tp_8/iter_0006000"
-prefix = "language_model."
-TP = 8
+        for k, v in state_dict["model"].items():
+            if "vision_model" in k:
+                new_state_dict["model"][k] = v
 
-# orig_dir = "/lustre/fs1/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/llama_3p1_8b_c-radio-vlm_v1_rc3-no-extra-state"
-# new_dir = "/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/mcore_mmodal_models/n5p5_12b_sft_0718_cradio_vlm_v1_rc3_tp4"
-# llm_dir = "/lustre/fsw/portfolios/llmservice/users/matthieul/workspace/output/nano-v2-sft-lr5e-6-128k-nollama-thinkfix-ep2-iter_0006000-path-vocab-tp_4/iter_0006000"
-# prefix = "language_model."
-# TP = 4
+        llm_state_dict = torch.load(llm_path, weights_only=False)
+        for k, v in llm_state_dict["model"].items():
+            if "vision_model" not in k:
+                new_state_dict["model"][f"{prefix}{k}"] = v
 
-for i in range(TP):
-    orig_path = os.path.join(orig_dir, "iter_0000001", f"mp_rank_0{i}", "model_optim_rng.pt")
-    new_path = os.path.join(new_dir, "iter_0000001", f"mp_rank_0{i}", "model_optim_rng.pt")
-    os.makedirs(os.path.join(new_dir, "iter_0000001", f"mp_rank_0{i}"), exist_ok=True)
-    llm_path = os.path.join(llm_dir, f"mp_rank_0{i}", "model_optim_rng.pt")
-
-    state_dict = torch.load(orig_path, weights_only=False)
-    new_state_dict = state_dict.copy()
-    new_state_dict["model"] = dict()
-
-    for k, v in state_dict["model"].items():
-        if "vision_model" in k:
-            new_state_dict["model"][k] = v
-
-    llm_state_dict = torch.load(llm_path, weights_only=False)
-    for k, v in llm_state_dict["model"].items():
-        if "vision_model" not in k:
-            new_state_dict["model"][f"{prefix}{k}"] = v
-
-    torch.save(new_state_dict, new_path)
-    print("done", i)
+        torch.save(new_state_dict, new_path)
+        print("done", i)
