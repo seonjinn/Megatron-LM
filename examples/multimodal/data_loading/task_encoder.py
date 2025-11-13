b5ec9cd219c20a714ecfc30d21d05a81ca742f9f
Lukas Voegtle <lvoegtle@nvidia.com>
2025-11-13 07:33:58 -0800

    Add earlier failing if media cannot be loaded for a sample

--- examples/multimodal/data_loading/task_encoder.py
+++ examples/multimodal/data_loading/task_encoder.py
@@ -411,6 +411,10 @@ class MultiModalTaskEncoder(
                     if isinstance(fragment, (ImageMedia, VideoFrameMedia)):
                         # Image or a single frame
                         aggregated_num_frames.append(1)
+                    elif isinstance(fragment, bytes):
+                        raise ValueError(f"Could not convert bytes to known media type: {fragment[:100]!r}")
+                    else:
+                        raise ValueError(f"Unexpected media type: {type(fragment)}. Fragment: {fragment}")
                     idx += 1
 
         image_media: list[ImageMedia | VideoFrameMedia] = []
@@ -901,6 +905,8 @@ class MultiModalTaskEncoder(
                 elif isinstance(media_value, Image.Image):
                     for frame in frames:
                         frame.media.value = media_value
+                elif isinstance(media_value, bytes):
+                    raise ValueError(f"Unable to parse bytes as known media type: {media_value[:100]!r}")
                 else:
                     raise ValueError(f"Unexpected media type: {type(media_value)}. Media: {media}")
         else:
